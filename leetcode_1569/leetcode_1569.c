bc219770467a8a4e6298ef9918e6eb7d6936796f
Kananda Winter <kanandabw@gmail.com>
2025-08-01 14:41:25 -0300

    leetcode_1569 base

---
+++ leetcode_1569/leetcode_1569.c
@@ -0,0 +1,53 @@
+const int MOD = 1000000007; //modulo 10^9 + 7
+long long comb[1001][1001];
+
+void precomputeComb() {
+    // C(n,0) = 1 para todo n
+    for (int i = 0; i <= 1000; i++) {
+        comb[i][0] = 1;
+    }
+    
+    // C(n,k) = C(n-1,k-1) + C(n-1,k)
+    for (int n = 1; n <= 1000; n++) {
+        for (int k = 1; k <= n; k++) {
+            comb[n][k] = (comb[n-1][k-1] + comb[n-1][k]) % MOD;
+        }
+    }
+}
+
+long long solve(int* nums, int size) {
+    if (size <= 2) return 1;  // caso base: 0, 1 ou 2 elementos = 1 way
+    
+    int root = nums[0];  // primeiro elemento = sempre a raiz
+    
+    // separa em subárvores esquerda e direita
+    int left[1000], right[1000];
+    int leftSize = 0, rightSize = 0;
+    
+    for (int i = 1; i < size; i++) {
+        if (nums[i] < root) {
+            left[leftSize++] = nums[i];
+        } else {
+            right[rightSize++] = nums[i];
+        }
+    }
+    
+    //recursão das subárvores
+    long long waysLeft = solve(left, leftSize);
+    long long waysRight = solve(right, rightSize);
+    
+    // total = C(leftSize + rightSize, leftSize) * waysLeft * waysRight
+    long long totalWays = comb[leftSize + rightSize][leftSize];
+    totalWays = (totalWays * waysLeft) % MOD;
+    totalWays = (totalWays * waysRight) % MOD;
+    
+    return totalWays;
+}
+
+int numOfWays(int* nums, int numsSize) {
+    precomputeComb();
+    
+    // subtrai 1 porque a pergunta é "outras formas" que exclui o original
+    long long result = solve(nums, numsSize);
+    return (result - 1 + MOD) % MOD;
+}
